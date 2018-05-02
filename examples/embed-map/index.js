995e5640233c56661e62bf8c2c5a0a1fab05d6ff
livia <liviamelq@gmail.com>
2018-05-02 16:20:43 +1000

    Use prettify

--- examples/embed-map/index.js
+++ examples/embed-map/index.js
@@ -1,4 +1,4 @@
-function initMap(el, options){
+function initMap(el, options) {
   const map = new google.maps.Map(el, {
     center: options.cityCoordinates,
     zoom: 12
@@ -12,7 +12,7 @@ function initMap(el, options){
 
 const views = {
   embedMap: function embedMap(el, props) {
-    google.maps.event.addDomListener(window, 'load', function onLoad () {
+    google.maps.event.addDomListener(window, "load", function onLoad() {
       initMap(el, props);
     });
   }
