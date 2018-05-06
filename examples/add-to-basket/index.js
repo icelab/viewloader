e97dec8406e30f8ceb379a766724e32e9ebc9190
livia <liviamelq@gmail.com>
2018-05-07 09:00:39 +1000

    add remove function to views

--- examples/add-to-basket/index.js
+++ examples/add-to-basket/index.js
@@ -1,25 +1,25 @@
 const views = {
-  remove : function remove(el) {
-    el.addEventListener('click', function () {
+  remove: function remove(el) {
+    el.addEventListener("click", function() {
       el.parentNode.remove();
-    })
+    });
   },
 
   addToBasket: function addToBasket(el, props) {
     el.addEventListener("click", function() {
       fetch(props.file)
-        .then((res) => res.text())
-        .then((data) => {
-          const wrapper = document.createElement('div');
-          wrapper.innerHTML = data
+        .then(res => res.text())
+        .then(data => {
+          const wrapper = document.createElement("div");
+          wrapper.innerHTML = data;
           const container = document.querySelector(`.${props.target}`);
           container.appendChild(wrapper);
 
           viewloader.execute(views, wrapper, true);
         })
-        .catch((err) => console.log(err));
+        .catch(err => console.log(err));
     });
   }
-}
+};
 
 viewloader.execute(views);
